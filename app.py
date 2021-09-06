3d4bc0fc30aaeef7f4b7c7dda5979f0c38e999b8
Guru Prasad Mullangi <guruprasadmullangi@gmail.com>
2021-09-06 13:48:20 -0400

    setting things up

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import logging
 import cbpro
 import re
 import sys
+import base64
 from datetime import datetime
 from os.path import join, dirname
 from dotenv import load_dotenv
@@ -15,14 +16,21 @@ load_dotenv(dotenv_path)
 logging.basicConfig(level=logging.DEBUG)
 
 COINBASE_API_KEY = os.environ.get('COINBASE_API_KEY')
+COINBASE_API_PASSPHARSE = os.environ.get('COINBASE_API_PASSPHARSE')
 COINBASE_API_SECRET = os.environ.get('COINBASE_API_SECRET')
 SLACK_BOT_TOKEN = os.environ.get('SLACK_BOT_TOKEN')
 SLACK_APP_TOKEN = os.environ.get('SLACK_APP_TOKEN')
 
 app = App(token=SLACK_BOT_TOKEN)
+auth_client = cbpro.AuthenticatedClient(COINBASE_API_KEY, COINBASE_API_SECRET, COINBASE_API_PASSPHARSE, api_url="https://api.pro.coinbase.com")
 
 @app.command('/help')
 def help(ack, command):
+    ack(f"Hello <@{command['user_id']}>")
+
+@app.command('/balance')
+def help(ack, command, logger):
+    logger.info(auth_client.get_accounts())
     ack(f"Hello <@{command['user_id']}>")
 
 if __name__ == '__main__':
