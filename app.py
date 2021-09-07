d676ad385ad953e920318792d2f36d7525a2600c
Guru Prasad Mullangi <guruprasadmullangi@gmail.com>
2021-09-06 23:26:34 -0400

    added skeleton porl

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import json
 import cbpro
 from datetime import datetime
 from os.path import join, dirname
@@ -17,6 +18,9 @@ COINBASE_API_PASSPHARSE = os.environ.get('COINBASE_API_PASSPHARSE')
 COINBASE_API_SECRET = os.environ.get('COINBASE_API_SECRET')
 SLACK_BOT_TOKEN = os.environ.get('SLACK_BOT_TOKEN')
 SLACK_APP_TOKEN = os.environ.get('SLACK_APP_TOKEN')
+
+pd = os.environ.get('pd')
+pd_list = json.loads(pd)
 
 app = App(token=SLACK_BOT_TOKEN)
 auth_client = cbpro.AuthenticatedClient(COINBASE_API_KEY, COINBASE_API_SECRET, COINBASE_API_PASSPHARSE, api_url="https://api.pro.coinbase.com")
@@ -36,10 +40,28 @@ def help(ack, command):
             balance = account['balance']
             available = account['available']
             ticker_price = public_client.get_product_ticker(product_id=currency+'-USD')
-            msg = msg + "{:<8} {:<30} {:<20} {:<20} {:<20}".format(currency, balance, available, ticker_price['price'], float(available) * float(ticker_price['price'])) + '\n'
+            msg = msg + "{:<8} {:<30} {:<20} {:<20} {:<30}".format(currency, balance, available, ticker_price['price'], float(available) * float(ticker_price['price'])) + '\n'
 
     msg = msg + '```'
     ack(f"Your accounts: \n\n{msg}")
+
+@app.command('/porl')
+def help(ack, command):
+    accounts = auth_client.get_accounts()
+    msg = '```'
+    for account in accounts:
+        if float(account['balance']) > 0.0000000000000000:
+            currency = account['currency']
+            available = account['available']
+            total_deposit = float(pd_list[currency])
+            ticker_price = public_client.get_product_ticker(product_id=currency+'-USD')
+            current_total = float(available) * float(ticker_price['price'])
+            porl = current_total - total_deposit
+            msg = msg + "{:<10} {:<10}".format(currency, porl) + '\n'
+    
+    msg = msg + '```'
+    ack(f"Profits and losses: \n\n{msg}")
+          
 
 if __name__ == '__main__':
     SocketModeHandler(app, SLACK_APP_TOKEN).start()
